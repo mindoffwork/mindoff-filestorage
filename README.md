9a08beececaaec7e4cf9af32736afcf4db560667
Joel Samraj <joelsamrajs@gmail.com>
2024-10-10 19:52:03 +0530

    Added basic export functionality for spreadjsoninator.py

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # mindoffwork-posts
+
+## Project Structure and It's Purposes
+mindoffwork-posts
+├── public                           # Static files like images, fonts, and favicon
+│   ├── data
+|   |   └── topics
+            └── topicslist.json 
+│   │   |   └──{topic-1}.json
+|   |   |   └──{...}
+│   │   └── pages
+|   |     
+│   └── images 
+|   │   └── {image-1}.{png}                   
+|   │   └── {...} 
+├── posts   
+│   └── {post-1}  
+|   |   └── {post-1}.json        
+|   |   └── {post-1}.md  
+|   └── {...}                 
+└── db_sheet.xlsx
+└── spreadjsoninator.py
---
+++ spreadjsoninator.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+# Load the Excel file
+excel_file_path = 'db_sheet.xlsx'  # Update this with your file path
+sheet_name = 'topics'  # Name of the sheet
+
+# Read the Excel file
+df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+df.columns = df.columns.str.lower()
+df = df.astype(str)
+
+# Export the DataFrame to a JSON file
+output_directory = f'public/{sheet_name}/'  # Update this with desired output directory
+output_file_path = os.path.join(output_directory, 'output_file.json')  # Change the output file name as needed
+
+# Create the directory if it doesn't exist
+os.makedirs(output_directory, exist_ok=True)
+df.to_json(output_file_path, orient='records', lines=False, compression='gzip')
+
+print(f"Data exported to \'{output_file_path}\' successfully.")
