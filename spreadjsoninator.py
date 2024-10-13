e84725fbeba891160b2a0cf21eb67abb6a1ccdb9
Joel Samraj <joelsamrajs@gmail.com>
2024-10-13 22:29:17 +0530

    Added Home Posts and Restructured Dist export

--- spreadjsoninator.py
+++ spreadjsoninator.py
@@ -1,45 +1,52 @@
 import os
 import pandas as pd
-
+import json
 # Load the Excel file
 excel_file_path = 'db_sheet.xlsx'  # Update this with your file path
+output_directory = 'dist/'
+files_deleted= 0
+for filename in os.listdir(output_directory):
+    file_path = os.path.join(output_directory, filename)
+    if os.path.isfile(file_path):
+        os.remove(file_path) 
+        files_deleted += 1
+print(f"Deleted successfully -- {files_deleted} files")
 
-# 1. Export TopicsList JSON
-sheet_name = 'topics'  # Name of the sheet
+# 1. Export Topics JSON
+sheet_name = 'topics'
 topics_df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
 topics_df.columns = topics_df.columns.str.lower()
 topics_df = topics_df.astype(str)
-
-# Export the DataFrame to a JSON file
-output_directory = f'dist/{sheet_name}/'  # Update this with desired output directory
-output_file_path = os.path.join(output_directory, 'topicslist.json')  # Change the output file name as needed
-
-# Create the directory if it doesn't exist
+output_file_path = os.path.join(output_directory, 'topicslist.json')  
 os.makedirs(output_directory, exist_ok=True)
 topics_df.to_json(output_file_path, orient='records', lines=False)
+print("Exported successfully -- Topics")
 
-print("Topics List exported successfully.")
 
-# 2. Export Invidual Topic JSON
+# 2. Export POSTS JSON 
 sheet_name = 'posts'
 posts_df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
 posts_df.columns = posts_df.columns.str.lower()
-
-# Export the DataFrame to a JSON file
-output_directory = f'dist/{sheet_name}/'  # Update this with desired output directory
-output_file_path = os.path.join(output_directory, 'topicslist.json')  # Change the output file name as needed
-
-# 1. Get the unique topics as List
-topic_list = posts_df['topic'].unique()
+posts_df['created_on'] = posts_df['created_on'].dt.strftime('%Y-%m-%d')
 posts_df['updated_on'] = posts_df['updated_on'].dt.strftime('%Y-%m-%d')
+
+# 2a. Topicwise Posts JSON
+topic_list = posts_df['topic'].unique()
 for topic in topic_list:
     filtered_by_topic_df = posts_df[posts_df['topic'] == topic]
-    output_directory = f'dist/{sheet_name}/'  # Update this with desired output directory
-    output_file_path = os.path.join(output_directory, f'{topic}.json')  # Change the output file name as needed
-    
-    # Create the directory if it doesn't exist
+    filtered_by_topic_df = filtered_by_topic_df.drop(['is_featured', 'is_popular', 'is_recommended'], axis=1)
+    output_file_path = os.path.join(output_directory, f'posts_topic__{topic}.json')
     os.makedirs(output_directory, exist_ok=True)
     filtered_by_topic_df.to_json(output_file_path, orient='records', lines=False)
+print("Exported successfully -- Topicwise Posts")
 
-    print(f"Posts Exported successfully for '{topic}'")
-    
+# 2b. HomePage Posts Json
+homepage_posts_list = {
+    "is_featured": posts_df[posts_df["is_featured"]].to_dict(orient='records'),
+    "is_popular": posts_df[posts_df["is_popular"]].to_dict(orient='records'),
+    "is_recommended": posts_df[posts_df["is_recommended"]].to_dict(orient='records'),
+}
+# Write to JSON file
+with open(output_directory + 'posts_home.json', 'w') as json_file:
+    json.dump(homepage_posts_list, json_file)
+print("Exported successfully -- Home Posts")
