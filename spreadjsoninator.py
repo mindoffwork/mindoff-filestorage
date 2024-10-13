ee959de198d083827eb42f4f9d79a4bea8a87f3d
Joel Samraj <joelsamrajs@gmail.com>
2024-10-14 00:40:57 +0530

    added posts export functionality

--- spreadjsoninator.py
+++ spreadjsoninator.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import json
+
+
 # Load the Excel file
 excel_file_path = 'db_sheet.xlsx'  # Update this with your file path
 output_directory = 'dist/'
@@ -23,14 +25,15 @@ topics_df.to_json(output_file_path, orient='records', lines=False)
 print("Exported successfully -- Topics")
 
 
-# 2. Export POSTS JSON 
+# 2. Export POSTLIST JSON 
 sheet_name = 'posts'
 posts_df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
 posts_df.columns = posts_df.columns.str.lower()
+posts_df['id'] = posts_df['id'].astype(str)
 posts_df['created_on'] = posts_df['created_on'].dt.strftime('%Y-%m-%d')
 posts_df['updated_on'] = posts_df['updated_on'].dt.strftime('%Y-%m-%d')
 
-# 2a. Topicwise Posts JSON
+# 2a. Topicwise Postlist JSON
 topic_list = posts_df['topic'].unique()
 for topic in topic_list:
     filtered_by_topic_df = posts_df[posts_df['topic'] == topic]
@@ -40,7 +43,7 @@ for topic in topic_list:
     filtered_by_topic_df.to_json(output_file_path, orient='records', lines=False)
 print("Exported successfully -- Topicwise Posts")
 
-# 2b. HomePage Posts Json
+# 2b. HomePage Postlist Json
 homepage_posts_list = {
     "is_featured": posts_df[posts_df["is_featured"]].to_dict(orient='records'),
     "is_popular": posts_df[posts_df["is_popular"]].to_dict(orient='records'),
@@ -50,4 +53,17 @@ homepage_posts_list = {
 with open(output_directory + 'posts_home.json', 'w') as json_file:
     json.dump(homepage_posts_list, json_file)
 print("Exported successfully -- Home Posts")
+
+# 3. Export POSTS JSON 
+# Generate JSON-LD for each row and save to individual JSON files
+output_directory = 'posts/'
+for index, row in posts_df.iterrows():
+    # Construct the file path
+    output_file_path = os.path.join(output_directory, row['link'], 'post.json')
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    row.to_json(output_file_path, orient='records', lines=False)
+print("Exported successfully -- Posts")
 print("Process Completed 😇")
+
+
+### SUPPORTING FUNCTIONS ###
