653c7bc74aa35cd312e341e008d822be36784c9b
Joel Samraj <joelsamrajs@gmail.com>
2024-10-16 20:37:57 +0530

    Auto commit: 2024-10-16 20:37:57

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Run Python file
-python3 spreadjsoninator.py
+# Activate the virtual environment
+source ./.venv/Scripts/activate  # Assuming the virtual environment is in the same folder
+
+# Run the Python file
+python spreadjsoninator.py  # Assuming script.py is in the same folder as the shell script
+
+# Deactivate the virtual environment
+deactivate
 
 # Git commands
 git add .
