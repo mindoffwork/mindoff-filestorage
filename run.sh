04e7c56dc5e5a6e41e17a218eb4df804be870a0f
Joel Samraj <joelsamrajs@gmail.com>
2024-10-25 20:24:27 +0530

    Auto commit: 2024-10-25 20:24:27

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ./.venv/Scripts/activate  # Assuming the virtual environment is in the same folder
+
+# Run the Python file
+python spreadjsoninator.py  # Assuming script.py is in the same folder as the shell script
+
+# Deactivate the virtual environment
+deactivate
