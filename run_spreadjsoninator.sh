2fd46d1b29f8f9f889934472a11a7e9e44cf6fed
Joel Samraj <165904090+mindoffwork@users.noreply.github.com>
2024-12-22 01:42:06 +0530

    Auto commit: 2024-12-22 01:42:06

--- run_spreadjsoninator.sh
+++ run_spreadjsoninator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source ./.env/Scripts/activate  # Assuming the virtual environment is in the same folder
+source ./.venv/Scripts/activate  # Assuming the virtual environment is in the same folder
 
 # Run the Python file
 python spreadjsoninator.py  # Assuming script.py is in the same folder as the shell script
